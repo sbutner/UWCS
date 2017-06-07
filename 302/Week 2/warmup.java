d3cc91925db6c167681e02c725b538ec28941746
Sean Butner <sean.butner@gmail.com>
2017-06-07 17:51:11 -0500

    Fix errors in the warmup exercise. It now executes in java visualizer

--- 302/Week 2/warmup.java
+++ 302/Week 2/warmup.java
@@ -8,65 +8,48 @@ public class ClassNameHere {
       System.out.println("Welcome to HeathScore");
       System.out.println("Please answer the following 3 questions.");
       
-      Scanner stdin = new Scanner (System.in);
+      Scanner stdin = new Scanner(System.in);
       // a base score will be 10
-      int score = 10  
-      System.out.println ("your starting score is " + score);
+      int score = 10;  
+      System.out.println("your starting score is " + score);
       
       
-      System.out.println("What is your good (HDL) cholestol number?");
+      System.out.println("What is your good (HDL) cholesterol number?");
       int chol = stdin.nextInt();
       if (chol > 60)
-      {
          score = score + 5;
-      }
       else if (chol >= 40)
-      {
          score = score + 1;
-      }
       else
          score = score - 1;
-      System.out.println ("your score is curently " + scre);
+      System.out.println("your score is currently " + score);
       
       
       
       System.out.println("What is your fasting glucose level?");
       int glucose = stdin.nextInt();
       if (glucose < 80)
-      {
          score = score + 5;
-      }
       else if (glucose < 100)
-      {
          score = score + 1;
-      }
-      else (glucose < 125)
-      {
+      else if (glucose < 125)
          score = score - 2;
-      }
       else
-      {
          score = score - 5;
-      }
-      System.out.println ("your score is curently " + score);
-      
+      System.out.println("your score is currently " + score);
             
       System.out.println("Do you smoke? (enter 1 for yes and 2 for no)");
       int smoker = stdin.nextInt();
       if (smoker == 1)
-      {
          score = score - 10;
-      }
       else
-      {
          score = score + 1;
-      }
       
       System.out.println ("your final score is  " + score);
       
       if (score > 20)
          System.out.println("You are in great health!!");
-      else if (score  16)
+      else if (score > 16)
          System.out.println("You are good health, but you can improve.");
       else if (score > 10)
          System.out.println("You need to make some changes");
